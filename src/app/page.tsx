b16e0bd92041208bd567ec39e1c70182f6d0b8a5
Aneesh R Rao <aneeshrao12@outlook.com>
2025-07-20 15:42:52 +0530

    trpc setup success

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+// "use client"
 import { Button } from "@/components/ui/button";
-
-const Page = () => {
-  return (<div className=""><Button variant={"destructive"}>click me</Button></div>);
+import { useTRPC } from "@/trpc/client";
+import { caller, getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary, useQuery } from "@tanstack/react-query";
+import { Suspense } from "react";
+const Page = async  () => {
+  return <div>hello</div>
 }
 
 export default Page;
