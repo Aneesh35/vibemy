ce4e688e3e3dd37dda2e4db26bc7d7abc4bf3f79
Aneesh R Rao <aneeshrao12@outlook.com>
2025-07-27 23:35:05 +0530

    configured e2b sandbox-environment

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import { inngest } from "./client";
 import { openai, createAgent } from "@inngest/agent-kit";
+import {Sandbox} from "@e2b/code-interpreter"
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event,step }) => {
+    const sandboxId=await step.run("get-sandbox-id",async()=>{
+      const sandbox=await Sandbox.create("vibemy-test2")
+      return sandbox.sandboxId;
+    });
+
     const codeAgent = createAgent({
       name: 'Code-agent',
       system:
@@ -12,6 +19,13 @@ export const helloWorld = inngest.createFunction(
       model: openai({ model: 'gpt-4o-mini' }),
     });
     const { output } = await codeAgent.run(`summarize the following topic ${event.data.prompt}`);
-    return { output };
+
+    const sandboxUrl=await step.run("get-sandbox-url",async()=>{
+      const sandbox=await getSandbox(sandboxId);
+      const host=sandbox.getHost(3000);
+      return `https://${host}`;
+    })
+    
+    return { output,sandboxUrl };
   }
 );
