689d38d732a1356e78de83b300b5541472237639
Aneesh R Rao <aneeshrao12@outlook.com>
2025-07-30 21:27:03 +0530

    added prisma-Messages schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,23 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { toast } from "sonner";
 
 const Page = () => {
   const trpc = useTRPC();
-  const [prompt, setPrompt] = useState<string>("");
-  const invoke = useMutation(trpc.invoke.mutationOptions({onSuccess:()=>{
-    toast.success("background job started!")
+  const [value, setValue] = useState<string>("");
+  const {data:messages}=useQuery(trpc.messages.getMany.queryOptions())
+  const createMessage = useMutation(trpc.messages.create.mutationOptions({onSuccess:()=>{
+    toast.success("messages created")
   }}))
   return <div className="p-4 max-w-7xl mx-auto">
-    <Input onChange={(e)=>setPrompt(e.target.value)} value={prompt} />
-    <Button disabled={invoke.isPending} onClick={()=>invoke.mutate({prompt:prompt})}>
+    <Input onChange={(e)=>setValue(e.target.value)} value={value} />
+    <Button disabled={createMessage.isPending} onClick={()=>createMessage.mutate({value:value})}>
       Invoke the background Job
     </Button>
+    {JSON.stringify(messages)}
   </div>
 }
 
